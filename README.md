46841ee2319324de6fdd8c3c3249ce40f502a7ef
luke <luke2work@gmail.com>
2023-09-19 17:57:57 +0800

    all file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# moriplan_staging
+moriplan
+森空間
+
+#note :
+admin make controller
+example -> 
+php artisan admin:make UserController --model=App\Models\User
+php artisan admin:make BannerController –-model=App\Models\Banner
+請用手KEY別忘了
+
+
+
---
+++ vendor/laravel-admin-ext/api-tester/src/ApiTesterController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Encore\Admin\ApiTester;
+
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Arr;
+
+class ApiTesterController extends Controller
+{
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('Api tester');
+
+            $tester = new ApiTester();
+
+            $content->body(view('api-tester::index', [
+                'routes' => $tester->getRoutes(),
+//                'logs'   => ApiLogger::load(),
+            ]));
+        });
+    }
+
+    public function handle(Request $request)
+    {
+        $method = $request->get('method');
+        $uri = $request->get('uri');
+        $user = $request->get('user');
+        $all = $request->all();
+
+        $keys = Arr::get($all, 'key', []);
+        $vals = Arr::get($all, 'val', []);
+
+        ksort($keys);
+        ksort($vals);
+
+        $parameters = [];
+
+        foreach ($keys as $index => $key) {
+            $parameters[$key] = Arr::get($vals, $index);
+        }
+
+        $parameters = array_filter($parameters, function ($key) {
+            return $key !== '';
+        }, ARRAY_FILTER_USE_KEY);
+
+        $tester = new ApiTester();
+
+        $uri = env('API_DOMAIN').$uri;
+
+        $response = $tester->call($method, $uri, $parameters, $user);
+
+        return [
+            'status'    => true,
+            'message'   => 'success',
+            'data'      => $tester->parseResponse($response),
+        ];
+    }
+}
